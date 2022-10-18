8effb7454065eebc5b8b06b4751cbb85ef955e1e
Jackson Harris <109924053+Jaxnbh47@users.noreply.github.com>
2022-10-17 21:26:55 -0400

    master

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-for (let i = 0; i <= 5; i++){
-    console.log(`The value of i= ${i} is less than or equal to 5`)
-    }
-    
-    for (let i = 10; i > 0; i--){
-        console.log(`The value of i= ${i} 
-        is less than or equal to 10`)
-    }
-    
-    for (let i = 0; i < 20; i+=2){
-        console.log(`The value of i= ${i} is less than or equal to 18`)
-    }
+for(let i = 1; i < 10; i++){
+console.log(`${i}`)
+}
+
+for (let i = 10; i > 0; i--){
+    console.log(`${i}`)
+}
