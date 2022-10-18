f9b4f51ccd909b1c61400c981c9d97238cf8468b
Jackson Harris <109924053+Jaxnbh47@users.noreply.github.com>
2022-10-17 21:54:34 -0400

    master

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ console.log(`${i}`)
 for (let i = 10; i > 0; i--){
     console.log(`${i}`)
 }
+
+let i = 0;
+while (i < 10) {
+  i++;
+  console.log(i);
+} 
+
+let j = 11;
+while (j > 0) {
+  j--;
+  console.log(j)
+} 
